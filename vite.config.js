d90bc8cbb64731e66334c145de4c8273c0bd59f1
zhoubolin0404 <zhoubolin0404@126.com>
2026-09-04 14:20:21 +0800

    configure vite base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/experiment/",
   plugins: [react()],
-  base: '/experiment-frontend/', // <--- 关键：必须加上这行，名字要和仓库名一致
 })
